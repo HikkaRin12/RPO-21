10a5a7e0b39bb27d2563de7188fc105cd4b403a6
HikkaRin12 <operas444@gmail.com>
2026-03-21 19:23:27 +0100

    add JS lesson

---
+++ JavaScript/Lesson1/script.js
@@ -0,0 +1,60 @@
+// const name = "Adil";
+// const PI = 3.14;
+// const _password = "12345678";  
+
+// let a =45;
+// a= 75;
+// let c= a + a;
+// a = "Sophia";
+
+// let d = a+"@gmail.com";
+// var age = 26;
+
+// document.body.innerHTML = name + age;  
+// console.log(age);
+
+
+// alert("Hello, World!");
+// confirm("Are you sure you want to proceed?");
+// console.log("Hello, World!");
+
+// const name = "Adil";
+// // name = "Sophia"; // This will cause an error because const variables cannot be reassigned
+// console.log(name);
+
+// let a = 15;
+// a= "Amir"; // This is allowed because let variables can be reassigned
+// // console.log(a);
+
+// document.body.innerHTML += "<h2 class='title'>Adil</h2> ";
+// document.body.innerHTML += `<h2 class='title'>${a}</h2> `;
+
+
+// let a = 15;
+// let b = "Adil";
+// let c = true;
+// let d = "45";
+// console.log(a, typeof (a));
+// console.log(b, typeof (b));
+// console.log(c, typeof (c));
+// console.log(d, typeof (d));
+
+// prompt("Write your name psl)");
+// let userName = prompt("Write your name psl)");
+// document.body.innerHTML += `<h2 class='title'>Hello, ${userName}!</h2> `;
+// let userAge = prompt("Введите свой возраст:");
+// console.log(userAge, typeof (userAge));
+
+// console.log(5+10); // 15
+// console.log("Hello"+" "+"Adil"); // Hello Adil
+// console.log("5"+10); // 510
+// console.log("5"+"10"); // 510
+// console.log(5*"2"); // 10
+// console.log("Hello" - "Farid"); //Not a Number
+// console.log(typeof(NaN)); //number
+// console.log("Adil" * 5)//NaN
+// console.log("Adil" + true); //Adiltrue
+// console.log(operand + operand);
+let a = 5
+console.log(a+=1);
+console.log(a++); //6 a = a + 1
