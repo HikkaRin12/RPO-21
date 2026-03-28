d65f33cb709b3047f495871a5006fe19c098710e
HikkaRin12 <operas444@gmail.com>
2026-03-28 17:32:30 +0100

    New lesson 3

---
+++ JavaScript/Lesson3/script.js
@@ -0,0 +1,47 @@
+// let a = 0;
+// let b = 1;
+// let c = 5;
+// console.log(a,Boolean(a));//false
+// console.log(b, Boolean(b));//true
+// console.log(c,Boolean(c)); //true
+// let str1 = '';
+// let str2 = 'Adil'
+// console.log(Boolean(str1));
+// console.log(Boolean(str2));
+// let user = prompt("Enter month number");
+// if (user === 1 || user === 2 || user === 12 ){
+// console.log("Winter");
+// }else if (user === 3 || user === 4 || user === 5 ){
+//     console.log("Spring");
+// }else if (user === 6 || user === 7 || user === 8 ){
+//     console.log("Summer");
+// }else if (user === 9 || user === 10 || user === 11 ){
+//     console.log("Autumn");
+// }else{
+//     console.log("Correct month number");
+// }; 
+// document.body.innerHTML = `<h1>${user}</h1>`;
+// ! not функция
+// let str = " ";
+// // if (str.length != 0){ //true
+// //     console.log("строка не пуста ");
+// // }
+// if (!str){ //false
+//     console.log("please fill the string"); // !str - это не str, то есть если str пустая строка, то будет true, и мы выведем сообщение "please fill the string"
+// }else{
+//     console.log("строка не пуста ");// если str не пустая строка, то !str будет false, и мы перейдем к else блоку, где выведем сообщение "строка не пуста".
+// }
+let age = 18;
+let hasTicket = true;
+// if (age >= 18 && hasTicket) {
+//     console.log("Wlcome");
+// } else {
+//     console.log("Sorry");
+// }
+// let result =
+//   age >= 18 && hasTicket ? console.log("Welcome") : console.log("Sorry"); //Тернарный оператор
+// document.body.innerHTML = `<h1>${result}</h1>`; //Тернарный оператор в шаблонной строке
+// тернарный оператор лучше использовать для простых условий,
+// а для более сложных условий лучше использовать if-else, так как он более читаемый и понятный.
+// document.body.innerHTML = `<h1>${age >= 18 && hasTicket ? "Welcome" : "Sorry"}</h1>`; //Тернарный оператор в шаблонной строке
+
